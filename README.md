80fcc536d39eb54b2c7cb65fad258ff96525a950
Cristian Dulcey <cristian@loducode.com>
2020-07-04 22:13:03 -0500

    commit initial

---
+++ README.md
@@ -0,0 +1,10 @@
+# Library non Official for maytapi PYTHON
+
+Functionalities
+
+Send message
+Send multimedia
+Send contact
+Send location
+Send link
+Set Webhook
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'loducode_maytapi',
+  packages = ['loducode_maytapi'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Non official maytapi library python',
+  author = 'Cristian Dulcey',
+  author_email = 'cristian@loducode.com',
+  url = 'https://github.com/UnTalDulcey/loducode_maytapi', # use the URL to the github repo
+  download_url = 'https://github.com/UnTalDulcey/loducode_maytapi/tarball/0.1',
+  keywords = ['maytapi', 'whatsapp', 'loducode'],
+  classifiers = [],
+)
