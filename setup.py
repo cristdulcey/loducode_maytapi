44db44abedd78f98f1f3867df09351916e20ac00
Cristian Dulcey <cristian@loducode.com>
2020-07-04 22:26:31 -0500

    modificaciones: sin prints y devuelve un array al momento de cambiar el webhook

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'loducode_maytapi',
   packages = ['loducode_maytapi'], # this must be the same as the name above
-  version = '0.1',
+  version = '0.1.1',
   description = 'Non official maytapi library python',
   author = 'Cristian Dulcey',
   author_email = 'cristian@loducode.com',
